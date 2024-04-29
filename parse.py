ce8356f814876f1fcbed081a51ead02148f1d8dd
Maksym Operchuk <operlast529@gmail.com>
2024-04-29 18:40:25 +0300

    Create: get_page_content function in parse.py.

--- parse.py
+++ parse.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 
 BASE_URL = "https://www.mcdonalds.com/ua/uk-ua/eat/fullmenu.html"
 
+def get_page_content(url: str):
+    return requests.get(url).content
+
 
 def get_page_soup(page: bytes | str) -> BeautifulSoup:
     soup = BeautifulSoup(page, "html.parser")

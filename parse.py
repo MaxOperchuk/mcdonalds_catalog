53eb2314c48699db2f7bd2e9cca935e6113b0665
Maksym Operchuk <operlast529@gmail.com>
2024-04-29 12:20:59 +0300

    Create: BASE_URL variable in parse.py file.

--- parse.py
+++ parse.py
@@ -1,3 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+BASE_URL = "https://www.mcdonalds.com/ua/uk-ua/eat/fullmenu.html"
+
+
 def get_page_soup(page: bytes | str) -> BeautifulSoup:
     soup = BeautifulSoup(page, "html.parser")
     return soup

20103603c5366d2f11f0fbd8d171bc0fafa6c735
Maksym Operchuk <operlast529@gmail.com>
2024-04-29 18:43:25 +0300

    Create: get_nutrition_elements & extract_nutrition_elements functions in parse.py.

--- parse.py
+++ parse.py
@@ -4,6 +4,16 @@ from bs4 import BeautifulSoup
 
 BASE_URL = "https://www.mcdonalds.com/ua/uk-ua/eat/fullmenu.html"
 
+def extract_nutrition_elements(elements: list) -> list:
+    extracted_elements = []
+
+    for element in elements:
+        text = element.select_one("span.sr-only.sr-only-pd").text
+        extracted_elements.append(remove_extra_spaces(text))
+
+    return extracted_elements
+
+
 def get_page_content(url: str):
     return requests.get(url).content
 
@@ -19,3 +29,10 @@ def get_name(soup: BeautifulSoup):
 
 def get_description(soup: BeautifulSoup) -> str:
     return soup.select_one("div.cmp-text").get_text(strip=True)
+
+def get_nutrition_elements(soup: BeautifulSoup) -> list:
+    nutrition_elements = soup.select(
+        ".cmp-nutrition-summary__heading-primary-item .value"
+    )
+    return extract_nutrition_elements(nutrition_elements)
+

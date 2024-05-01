5a6e55e105cd282ee9d31f4c1097fa02fb81cc80
Maksym Operchuk <operlast529@gmail.com>
2024-05-01 11:06:42 +0300

    Replace: needed functions from parse.py to services.py file.

--- services.py
+++ services.py
@@ -1,3 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def get_page_content(url: str) -> bytes:
+    return requests.get(url).content
+
+
+def click_btn(driver: webdriver, btn_id: str) -> None:
+    btn = WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.ID, btn_id))
+    )
+    driver.execute_script("arguments[0].scrollIntoView(true);", btn)
+    btn.click()
+
+
+def get_page_soup(page: bytes | str) -> BeautifulSoup:
+    soup = BeautifulSoup(page, "html.parser")
+    return soup
 
 
 def additional_request_handler(link: str) -> BeautifulSoup:

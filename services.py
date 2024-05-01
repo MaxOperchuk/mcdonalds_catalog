ac0d26778c98f28df1846c38f8491e5fe8591a7b
Maksym Operchuk <operlast529@gmail.com>
2024-05-01 11:04:39 +0300

    Create: formatter functions in services.py file.

--- services.py
+++ services.py
@@ -1,2 +1,17 @@
 def remove_extra_spaces(text: str):
     return " ".join(text.split())
+
+
+def nutrition_elements_formatter(string: str) -> str:
+    result = string.split()[0]
+
+    if "ккал" in result:
+        result = result.replace("ккал", "")
+
+    return result
+
+
+def components_formatter(string: str) -> str:
+    result = string.split(":")[1]
+    result = result.split()[0]
+    return result
